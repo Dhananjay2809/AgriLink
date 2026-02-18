ba0790e0e3795f8954d6a91acd93981f5bd2e4df
Dhananjay Pratap Singh <pratapsinghdhananjay8@gmail.com>
2026-02-18 16:43:32 +0530

    s

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -40,8 +40,8 @@ authRouter.post('/login' , async(req,res)=>{
         const token=await user.getJwtToken();
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"lax",
+            secure:true,
+            sameSite:"none",
             maxAge:24*60*60*1000 //1 day
         });
         res.status(200).send({
